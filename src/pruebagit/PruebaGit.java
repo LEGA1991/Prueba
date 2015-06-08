336918b1d5cba2916891617fcd1cb9fd9486b0e9
adiseño.2015 <adiseo.2015@192.168.56.1>
2015-06-08 10:59:05 -0600

    Tercer impresion en consola de un Hola Mundo

--- src/pruebagit/PruebaGit.java
+++ src/pruebagit/PruebaGit.java
@@ -17,6 +17,7 @@ public class PruebaGit {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
         System.out.println("¿Como estas?");
+        System.out.println("Hola Mundo 2");
     }
     
 }
