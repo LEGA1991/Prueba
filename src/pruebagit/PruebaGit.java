09d46f82a9f027a6a5d6b88c8e1a3b6d5d110bc7
EST1534809 <EST1534809@192.168.56.1>
2015-06-08 10:49:26 -0600

    Escribiendo "¿Como estas?"

--- src/pruebagit/PruebaGit.java
+++ src/pruebagit/PruebaGit.java
@@ -16,6 +16,7 @@ public class PruebaGit {
      */
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
+        System.out.println("¿Como estas?");
     }
     
 }
